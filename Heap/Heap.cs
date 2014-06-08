080bda606dd92602c737aa3029d32b0727d92ab6
Pavlo Oleshkevych <oleshkevych@gmail.com>
2014-06-08 10:59:52 +0300

    Created new lib

---
+++ Heap/Heap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    public class Heap<T>
+    {
+        private T[] _storage;
+        
+        public Heap()
+        {
+            _storage = new T[3];
+        }
+    }
+}
